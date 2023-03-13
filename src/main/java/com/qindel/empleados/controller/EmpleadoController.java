a61dd27dabacc8754f91e53d41ea8b1c53c749e9
alejandrovag <alejandrovag@ext.inditex.com>
2023-03-13 09:15:42 +0100

    Exportacion e importacion de Excell

--- src/main/java/com/qindel/empleados/controller/EmpleadoController.java
+++ src/main/java/com/qindel/empleados/controller/EmpleadoController.java
@@ -1,8 +1,10 @@
-package controller;
+package com.qindel.empleados.controller;
 
 import com.qindel.empleados.model.Empleado;
 import com.qindel.empleados.service.EmpleadoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,27 +12,37 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
-@RequiredArgsConstructor
 public class EmpleadoController {
-    private final EmpleadoService empleadoService;
+    @Autowired
+    private  EmpleadoService empleadoService;
 
     @PostMapping("/empleados")
+    @ResponseStatus(HttpStatus.OK)
     public void save(@RequestBody Empleado empleado){
         empleadoService.save(empleado);
     }
 
     @GetMapping("/empleados")
+    @ResponseStatus(HttpStatus.OK)
     public List<Empleado> findAll(){
         return empleadoService.findAll();
     }
 
     @GetMapping("/empleado/id")
+    @ResponseStatus(HttpStatus.OK)
     public Optional<Empleado> findById(@PathVariable long id){
         return empleadoService.findById(id);
     }
 
     @DeleteMapping("/empleado/id")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteById(@PathVariable long id){
         empleadoService.deleteById(id);
+    }
+
+    @PutMapping("/empleados")
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@RequestBody Empleado empleado){
+        empleadoService.save(empleado);
     }
 }
--- src/main/java/com/qindel/empleados/listadoEmpleados/ListadoEmpleadosApplication.java
+++ src/main/java/com/qindel/empleados/listadoEmpleados/ListadoEmpleadosApplication.java
@@ -1,13 +1,23 @@
 package com.qindel.empleados.listadoEmpleados;
 
 import com.qindel.empleados.excel.importar.ImportarExcelEmpleados;
+import com.qindel.empleados.model.Empleado;
+import com.qindel.empleados.controller.EmpleadoController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
-public class ListadoEmpleadosApplication {
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+public class ListadoEmpleadosApplication extends SpringBootServletInitializer {
 
 	public ListadoEmpleadosApplication() throws FileNotFoundException {
 	}
@@ -19,9 +29,16 @@ public class ListadoEmpleadosApplication {
 		System.out.println(out);
 		ImportarExcelEmpleados importarExcell;
 		ImportarExcelEmpleados importarExcelEmpleados = new ImportarExcelEmpleados();
-		importarExcelEmpleados.excelImport();
-
+		List<Empleado> empleados = importarExcelEmpleados.excelImport();
+//		for (Empleado empleado: empleados
+//			 ) {
+//			empleadoController.save(empleado);
+//		}
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources (ListadoEmpleadosApplication.class);
+	}
 
 }
